68b17f8941734054de3b8d7d7cce4f94c9d0b75d
Alexis Peters <info@alexispeters.de>
2022-03-22 09:27:03 +0100

    move

--- Assets/AAAPE/DOTS/Scripts/Scheduler/EndSimulationScheduler.cs
+++
@@ -1,9 +0,0 @@
-using Unity.Entities;
-using Unity.Jobs;
-
-namespace AAAPE.DOTS {
-
-    public class EndSimulationScheduler : Scheduler<EndSimulationEntityCommandBufferSystem> {
-       public EndSimulationScheduler(Unity.Jobs.JobHandle handle): base(handle) {}
-    } 
-}
